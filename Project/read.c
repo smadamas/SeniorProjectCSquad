ee7fe15059fab2f04b459bfc93407cf13dc83b8b
Ziyad Morsi <ziyad.morsi@loxleyservices.com>
2021-01-26 12:16:01 -0500

    read functionallity

--- Project/read.c
+++ Project/read.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
 
 struct pixel** readToBuff(char* imageName, char* buffName){
 	printf("\nReading %s into %s...\n", imageName, buffName);
+	int width, height, ch;
+	buffName = stbi_load(imageName, &width, &height, &ch, 0);
+	if(buffName == NULL){
+		printf("Error reading file\n");
+		exit(1);
+	}
+	printf("Done reading!");
+	stbi_write
+	stbi_image_free(buffName);
 	return NULL;
 }

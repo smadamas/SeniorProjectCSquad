73faacb70479fb2986297d85add93aeed500d9ba
Ziyad Morsi <ziyad.morsi@loxleyservices.com>
2021-02-09 21:10:57 -0500

    display and updated readme

--- Project/app.c
+++ Project/app.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <setjmp.h>
 
-struct buff{
+struct buff
+{
 	char imageName[30];
 	char name[30];
-	unsigned char* img;
+	char status[30];
+	unsigned char *img;
 	int width, height, channels;
 };
 
@@ -17,18 +20,17 @@ struct buff{
 #include "edge.c"
 #include "display.c"
 
-void addBuffer(struct buff buffer, struct buff* buffers, int* buffCount);
-struct buff buffSearch(char* buffName, struct buff* buffers, int buffCount);
-void printBuffer(struct buff* buffer, int buffCount);
+void addBuffer(struct buff buffer, struct buff *buffers, int *buffCount);
+struct buff buffSearch(char *buffName, struct buff *buffers, int buffCount);
+void printBuffer(struct buff *buffer, int buffCount);
 void printMenu();
 char *get_filename_ext(const char *filename);
-int check_types(char* ext, char* file_types[]);
+int check_types(char *ext, char *file_types[]);
 
-
-int main(int   argc, char** argv) {
+int main(int argc, char **argv)
+{
 	printf("Welcome to the UNIX Image Manipulation tool.\n");
 	printf("Type \"menu\" to view the list of commands or \"list\" to view your buffers.\n\n");
-
 
 	struct buff buffers[10];
 	int buffCount = 0;
@@ -36,107 +38,130 @@ int main(int   argc, char** argv) {
 	char p[100];
 	gets(p);
 
-	while(1){
+	while (1)
+	{
 		char *command;
 		char *imageName;
 		char *buffName;
 		char *amount;
 
-		command = strtok(p," ");
+		command = strtok(p, " ");
 
-
-		if(strcmp(command,"menu")==0){
-                        printMenu();
-                }
-		else if(strcmp(command, "read")==0){
-			imageName = strtok(NULL," ");
-			char* ext = get_filename_ext(imageName);
-			char* file_types[5] = {"jpeg","jpg","gif","tiff","png"}; //ALLOW NEW FILE TYPES HERE
+		if (strcmp(command, "menu") == 0)
+		{
+			printMenu();
+		}
+		else if (strcmp(command, "read") == 0)
+		{
+			imageName = strtok(NULL, " ");
+			char *ext = get_filename_ext(imageName);
+			char *file_types[5] = {"jpeg", "jpg", "gif", "tiff", "png"}; //ALLOW NEW FILE TYPES HERE
 			int approved = check_types(ext, file_types);
-			if(approved != 1 ){
+			if (approved != 1)
+			{
 				printf("Error: Image file type is not approved.\n");
-                exit(1);
-            } 
-			strtok(NULL," ");
-			buffName = strtok(NULL," ");
+				exit(1);
+			}
+			strtok(NULL, " ");
+			buffName = strtok(NULL, " ");
 			struct buff temp = readToBuff(imageName, buffName);
 			addBuffer(temp, buffers, &buffCount);
 		}
-		else if(strcmp(command, "write")==0){
-			buffName = strtok(NULL," ");
-                        strtok(NULL," ");
-                        imageName = strtok(NULL," ");
-			writeToImage(buffSearch(buffName, buffers,buffCount), imageName);
-		}
-		else if(strcmp(command, "list")==0){
-			printBuffer(buffers, buffCount);
-		}
-		else if(strcmp(command, "brighten")==0){
+		else if (strcmp(command, "write") == 0)
+		{
 			buffName = strtok(NULL, " ");
 			strtok(NULL, " ");
-			imageName = strtok(NULL," ");
+			imageName = strtok(NULL, " ");
+			writeToImage(buffSearch(buffName, buffers, buffCount), imageName);
+		}
+		else if (strcmp(command, "list") == 0)
+		{
+			printBuffer(buffers, buffCount);
+		}
+		else if (strcmp(command, "brighten") == 0)
+		{
+			buffName = strtok(NULL, " ");
+			strtok(NULL, " ");
+			imageName = strtok(NULL, " ");
 			strtok(NULL, " ");
 			amount = strtok(NULL, " ");
 			addBuffer(brighten(buffSearch(buffName, buffers, buffCount), imageName, true, atoi(amount)), buffers, &buffCount);
 		}
-		else if(strcmp(command, "darken")==0){
+		else if (strcmp(command, "darken") == 0)
+		{
 			buffName = strtok(NULL, " ");
 			strtok(NULL, " ");
-			imageName = strtok(NULL," ");
+			imageName = strtok(NULL, " ");
 			strtok(NULL, " ");
 			amount = strtok(NULL, " ");
 			addBuffer(brighten(buffSearch(buffName, buffers, buffCount), imageName, false, atoi(amount)), buffers, &buffCount);
-
 		}
-		else if(strcmp(command, "display")==0){
+		else if (strcmp(command, "display") == 0)
+		{
 			buffName = strtok(NULL, " ");
-			printf("in display");
-			displayImage(buffSearch(buffName, buffers, buffCount), argc, argv);
-			// displayImage(buffName);
+			char tem[10] = "temp.png";
+
+			struct buff temp = buffSearch(buffName, buffers, buffCount);
+			if (strcmp(temp.status, "false") != 0)
+			{
+				writeToImage(temp, tem);
+				displayImage(tem, argc, argv);
+			}
+
+			remove(tem);
 		}
-		else if(strcmp(command, "quit")==0){
+		else if (strcmp(command, "quit") == 0)
+		{
 			break;
 		}
-		else if(strcmp(command, "horizontal")==0 || strcmp(command, "vertical")==0){
-			char* type = strtok(NULL, " ");
-			imageName = strtok(NULL, " ");		
-			detectEdge(command, type ,imageName);
+		else if (strcmp(command, "horizontal") == 0 || strcmp(command, "vertical") == 0)
+		{
+			char *type = strtok(NULL, " ");
+			imageName = strtok(NULL, " ");
+			detectEdge(command, type, imageName);
 		}
-		else if(strcmp(command, "addition")==0 || strcmp(command, "subtraction")==0 || strcmp(command, "division")==0 || strcmp(command, "multiplication")==0){
-			strtok(NULL," ");
-			char* buff1 = strtok(NULL," ");
-			char* cmd = strtok(NULL," ");
-			char* buff2 = strtok(NULL," ");
-			if(strcmp(cmd,"+")==0){
-				addBuffer(add(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-					buffers, &buffCount);
+		else if (strcmp(command, "addition") == 0 || strcmp(command, "subtraction") == 0 || strcmp(command, "division") == 0 || strcmp(command, "multiplication") == 0)
+		{
+			strtok(NULL, " ");
+			char *buff1 = strtok(NULL, " ");
+			char *cmd = strtok(NULL, " ");
+			char *buff2 = strtok(NULL, " ");
+			if (strcmp(cmd, "+") == 0)
+			{
+				addBuffer(add(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
 			}
-			else if(strcmp(cmd,"-")==0){
-				addBuffer(subtract(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-						buffers, &buffCount);
+			else if (strcmp(cmd, "-") == 0)
+			{
+				addBuffer(subtract(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
 			}
-			else if(strcmp(cmd,"*")==0){
-				addBuffer(multiply(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-						buffers, &buffCount);
+			else if (strcmp(cmd, "*") == 0)
+			{
+				addBuffer(multiply(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
 			}
-			else if(strcmp(cmd,"/")==0){
-				addBuffer(divide(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-						buffers, &buffCount);
+			else if (strcmp(cmd, "/") == 0)
+			{
+				addBuffer(divide(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
 			}
-			else{
+			else
+			{
 				printf("\nError: Arithmetic syntax incorrect\n");
 			}
 		}
-		else {
-			printf("\nCommand not found or not supported, please try again.\n\n");
-		} 
+		else
+		{
+			printf("\nCommand not found or not supported, please type menu for list of commands.\n\n");
+		}
 
 		gets(p);
 	}
-
 }
 
-void printMenu(){
+void printMenu()
+{
 	printf("\n----- Commands -----\n");
 	printf("\"quit\"\n");
 	printf("\"list\"\n");
@@ -152,24 +177,29 @@ void printMenu(){
 	printf("\"<horizontal/vertical> <kirsch/prewitt/sobel> <image-name>\"\n\n");
 }
 
-
-void printBuffer(struct buff* buffers, int buffCount){
+void printBuffer(struct buff *buffers, int buffCount)
+{
 	printf("\n----- Buffers -----\n");
- 	for (int i = 0; i < buffCount; i++){
-                printf("%s contains %s\n", buffers[i].name, buffers[i].imageName);
-        }
+	for (int i = 0; i < buffCount; i++)
+	{
+		printf("%s contains %s\n", buffers[i].name, buffers[i].imageName);
+	}
 	printf("\n");
 }
 
-void addBuffer(struct buff buffer, struct buff* buffers, int* buffCount){
+void addBuffer(struct buff buffer, struct buff *buffers, int *buffCount)
+{
 	int k = -1;
-        for (int i = 0; i < *buffCount; i++){
-        	if(strcmp(buffers[i].name, buffer.name)==0){
-                        k = i;
-                	break;
-        	}
-        }
-	if(k == -1){
+	for (int i = 0; i < *buffCount; i++)
+	{
+		if (strcmp(buffers[i].name, buffer.name) == 0)
+		{
+			k = i;
+			break;
+		}
+	}
+	if (k == -1)
+	{
 		buffers[*buffCount].img = buffer.img;
 		strcpy(buffers[*buffCount].imageName, buffer.imageName);
 		strcpy(buffers[*buffCount].name, buffer.name);
@@ -177,8 +207,9 @@ void addBuffer(struct buff buffer, struct buff* buffers, int* buffCount){
 		buffers[*buffCount].height = buffer.height;
 		buffers[*buffCount].channels = buffer.channels;
 		(*buffCount)++;
-    }
-    else{
+	}
+	else
+	{
 		buffers[k].img = buffer.img;
 		strcpy(buffers[k].imageName, buffer.imageName);
 		strcpy(buffers[k].name, buffer.name);
@@ -190,26 +221,35 @@ void addBuffer(struct buff buffer, struct buff* buffers, int* buffCount){
 	printf("New buffer added\n\n");
 }
 
-struct buff buffSearch(char* buffName, struct buff* buffers, int buffCount){
-	for (int i = 0; i < buffCount; i++){
-                if(strcmp(buffers[i].name, buffName)==0){
-                       	return buffers[i];
-                }
-        }
+struct buff buffSearch(char *buffName, struct buff *buffers, int buffCount)
+{
+	for (int i = 0; i < buffCount; i++)
+	{
+		if (strcmp(buffers[i].name, buffName) == 0)
+		{
+			return buffers[i];
+		}
+	}
 	printf("Error: buffer not found.\n");
 	struct buff temp;
+	strcpy(temp.status, "false");
 	return temp;
 }
 
-char *get_filename_ext(const char *filename){
-    char *dot = strrchr(filename, '.');
-    if(!dot || dot == filename) return "";
-    return dot + 1;
+char *get_filename_ext(const char *filename)
+{
+	char *dot = strrchr(filename, '.');
+	if (!dot || dot == filename)
+		return "";
+	return dot + 1;
 }
-int check_types(char* ext, char* file_types[]){
-    for(int i =0; i<5; i++){
-        int temp = strcmp(ext,file_types[i]);
-        if(temp == 0) return 1;
-    }
-    return 0;
+int check_types(char *ext, char *file_types[])
+{
+	for (int i = 0; i < 5; i++)
+	{
+		int temp = strcmp(ext, file_types[i]);
+		if (temp == 0)
+			return 1;
+	}
+	return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SeniorProjectCSquad
 
 Welcome to out senior project! This project is an image reading/saving software accessible from the Unix command line. :)
+
+Dependencies neeeded:
+\*sudo apt-get install libgtk2.0-dev
+\*sudo apt-get update -y
+\*sudo apt-get install -y libgd-dev

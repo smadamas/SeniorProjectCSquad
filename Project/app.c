acf10d3b18d5e90992314b4e30062f33b03214d5
Ziyad Morsi <ziyad.morsi@loxleyservices.com>
2021-02-10 23:05:26 -0500

    few changes

--- Project/app.c
+++ Project/app.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct buff{
+struct buff
+{
 	char imageName[15];
 	char name[15];
-	unsigned char* img;
+	unsigned char *img;
 	int width, height, channels;
 };
 
@@ -14,88 +15,99 @@ struct buff{
 #include "arithmetics.c"
 #include "histoEQ.c"
 
-void addBuffer(struct buff buffer, struct buff* buffers, int* buffCount);
-struct buff buffSearch(char* buffName, struct buff* buffers, int buffCount);
-void printBuffer(struct buff* buffer, int buffCount);
+void addBuffer(struct buff buffer, struct buff *buffers, int *buffCount);
+struct buff buffSearch(char *buffName, struct buff *buffers, int buffCount);
+void printBuffer(struct buff *buffer, int buffCount);
 void printMenu();
 
-struct buff histogramEqualisation(struct buff a, char* buffName);
+struct buff histogramEqualisation(struct buff a, char *buffName);
 
-
-int main() {
+int main()
+{
 	printf("Welcome to the UNIX Image Manipulation tool.\n");
 	printf("Type \"menu\" to view the list of commands or \"list\" to view your buffers.\n\n");
-
 
 	struct buff buffers[10];
 	int buffCount = 0;
 
 	char p[100];
-	
+
 	//strcpy(p,p2);
 	fgets(p, 100, stdin);
-	
 
-
-	while(1){
+	while (1)
+	{
 		//gets is depreciated
-		
-		char * p2 = strtok(p,"\n");
+
+		char *p2 = strtok(p, "\n");
 		char *command;
 		char *imageName;
 		char *buffName;
-		char * p3 = p2;
+		char *p3 = p2;
 
-		command = strtok(p3," ");
+		command = strtok(p3, " ");
 
-
-		if(strcmp(command,"menu")==0){
-                        printMenu();
-                }
-		else if(strcmp(command, "read")==0){
-			imageName = strtok(NULL," ");
-			strtok(NULL," ");
-			buffName = strtok(NULL," ");
+		if (strcmp(command, "menu") == 0)
+		{
+			printMenu();
+		}
+		else if (strcmp(command, "read") == 0)
+		{
+			imageName = strtok(NULL, " ");
+			strtok(NULL, " ");
+			buffName = strtok(NULL, " ");
 			struct buff temp = readToBuff(imageName, buffName);
 			addBuffer(temp, buffers, &buffCount);
 		}
-		else if(strcmp(command, "write")==0){
-			buffName = strtok(NULL," ");
-                        strtok(NULL," ");
-                        imageName = strtok(NULL," ");
-			writeToImage(buffSearch(buffName, buffers,buffCount), imageName);
+		else if (strcmp(command, "write") == 0)
+		{
+			buffName = strtok(NULL, " ");
+			strtok(NULL, " ");
+			imageName = strtok(NULL, " ");
+			writeToImage(buffSearch(buffName, buffers, buffCount), imageName);
 		}
-		else if(strcmp(command, "list")==0){
+		else if (strcmp(command, "list") == 0)
+		{
 			printBuffer(buffers, buffCount);
 		}
-		else if(strcmp(command, "quit")==0){
+		else if (strcmp(command, "quit") == 0)
+		{
 			break;
 		}
-		else{
-			strtok(NULL," ");
-			char* buff1 = strtok(NULL," ");
-			char* cmd = strtok(NULL," ");
-			char* buff2 = strtok(NULL," ");
-			if(strcmp(cmd,"+")==0){
-				addBuffer(add(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-					buffers, &buffCount);
+		else
+		{
+			strtok(NULL, " ");
+			char *buff1 = strtok(NULL, " ");
+			char *cmd = strtok(NULL, " ");
+			char *buff2 = strtok(NULL, " ");
+			if (strcmp(cmd, "+") == 0)
+			{
+				addBuffer(add(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
 			}
-			else if(strcmp(cmd,"-")==0){
-                                addBuffer(subtract(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-                                        buffers, &buffCount);
-                        }
-			else if(strcmp(cmd,"*")==0){
-                                addBuffer(multiply(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-                                        buffers, &buffCount);
-                        }
-			else if(strcmp(cmd,"/")==0){
-                                addBuffer(divide(buffSearch(buff1, buffers,buffCount), buffSearch(buff2, buffers,buffCount), command),
-                                        buffers, &buffCount);
-                        }
-			else if((strcmp(buff1,"histeq")==0)){
-				addBuffer(histogramEqualisation(buffSearch(cmd, buffers, buffCount), command), buffers, &buffCount);
+			else if (strcmp(cmd, "-") == 0)
+			{
+				addBuffer(subtract(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
 			}
-			else{
+			else if (strcmp(cmd, "*") == 0)
+			{
+				addBuffer(multiply(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
+			}
+			else if (strcmp(cmd, "/") == 0)
+			{
+				addBuffer(divide(buffSearch(buff1, buffers, buffCount), buffSearch(buff2, buffers, buffCount), command),
+						  buffers, &buffCount);
+			}
+			else if ((strcmp(buff1, "histeq") == 0))
+			{
+				struct buff temp = buffSearch(cmd, buffers, buffCount);
+				// printf("values are %d %d %d", temp.height, temp.width, temp.channels);
+				addBuffer(histogramEqualisation(temp, command), buffers, &buffCount);
+			}
+			else
+			{
 				printf("\nError: command not found.\n");
 			}
 		}
@@ -103,10 +115,11 @@ int main() {
 		fgets(p, 100, stdin);
 	}
 
-   	return 0;
+	return 0;
 }
 
-void printMenu(){
+void printMenu()
+{
 	printf("\n----- Commands -----\n");
 	printf("\"quit\"\n");
 	printf("\"list\"\n");
@@ -119,48 +132,57 @@ void printMenu(){
 	printf("histEQ: \"<buffer_dest> := histeq <buffer>\"\n\n");
 }
 
-
-void printBuffer(struct buff* buffers, int buffCount){
+void printBuffer(struct buff *buffers, int buffCount)
+{
 	printf("\n----- Buffers -----\n");
- 	for (int i = 0; i < buffCount; i++){
-                printf("%s contains %s\n", buffers[i].name, buffers[i].imageName);
-        }
+	for (int i = 0; i < buffCount; i++)
+	{
+		printf("%s contains %s\n", buffers[i].name, buffers[i].imageName);
+	}
 	printf("\n");
 }
 
-void addBuffer(struct buff buffer, struct buff* buffers, int* buffCount){
+void addBuffer(struct buff buffer, struct buff *buffers, int *buffCount)
+{
 	int k = -1;
-        for (int i = 0; i < *buffCount; i++){
-        	if(strcmp(buffers[i].name, buffer.name)==0){
-                        k = i;
-                	break;
-        	}
-        }
-	if(k == -1){
-                buffers[*buffCount].img = buffer.img;
-                strcpy(buffers[*buffCount].imageName, buffer.imageName);
+	for (int i = 0; i < *buffCount; i++)
+	{
+		if (strcmp(buffers[i].name, buffer.name) == 0)
+		{
+			k = i;
+			break;
+		}
+	}
+	if (k == -1)
+	{
+		buffers[*buffCount].img = buffer.img;
+		strcpy(buffers[*buffCount].imageName, buffer.imageName);
 		strcpy(buffers[*buffCount].name, buffer.name);
 		buffers[*buffCount].width = buffer.width;
-        	buffers[*buffCount].height = buffer.height;
-        	buffers[*buffCount].channels = buffer.channels;
+		buffers[*buffCount].height = buffer.height;
+		buffers[*buffCount].channels = buffer.channels;
 		(*buffCount)++;
-        }
-        else{
-                buffers[k].img = buffer.img;
-                strcpy(buffers[k].imageName, buffer.imageName);
-                strcpy(buffers[k].name, buffer.name);
-                buffers[k].width = buffer.width;
-                buffers[k].height = buffer.height;
-                buffers[k].channels = buffer.channels;
+	}
+	else
+	{
+		buffers[k].img = buffer.img;
+		strcpy(buffers[k].imageName, buffer.imageName);
+		strcpy(buffers[k].name, buffer.name);
+		buffers[k].width = buffer.width;
+		buffers[k].height = buffer.height;
+		buffers[k].channels = buffer.channels;
 	}
 }
 
-struct buff buffSearch(char* buffName, struct buff* buffers, int buffCount){
-	for (int i = 0; i < buffCount; i++){
-                if(strcmp(buffers[i].name, buffName)==0){
-                       	return buffers[i];
-                }
-        }
+struct buff buffSearch(char *buffName, struct buff *buffers, int buffCount)
+{
+	for (int i = 0; i < buffCount; i++)
+	{
+		if (strcmp(buffers[i].name, buffName) == 0)
+		{
+			return buffers[i];
+		}
+	}
 	printf("Error: buffer not found.\n");
 	struct buff temp;
 	return temp;
--- Project/histoEQ.c
+++ Project/histoEQ.c
@@ -1,129 +1,133 @@
-// C program to perform histogram equalisation to adjust contrast levels 
+// C program to perform histogram equalisation to adjust contrast levels
 
-#include <fcntl.h> 
-#include <math.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-  
-// Function to perform histogram equalisation on an image 
+#include <fcntl.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function to perform histogram equalisation on an image
 // Function takes total rows, columns, input file
 
-struct buff histogramEqualisation(struct buff a, char* buffName) 
-{ 
-    // creating image pointer 
-    unsigned char* image; 
+struct buff histogramEqualisation(struct buff a, char *buffName)
+{
+    // creating image pointer
+    unsigned char image[40000];
 
     //Instantiating Rows and Cols
     int cols = a.width;
     int rows = a.height;
-    
-    // Declaring 2 arrays for storing histogram values (frequencies) and 
-    // new gray level values (newly mapped pixel values as per algorithm) 
-    int hist[256] = { 0 }; 
-    int new_gray_level[256] = { 0 }; 
-  
-    // Declaring other important variables 
-    int col, row, total, curr, i; 
-  
-    // allocating image array the size equivalent to number of columns 
-    // of the image to read one row of an image at a time 
-    image = (unsigned char*)calloc(cols, sizeof(unsigned char)); 
-  
-    // Calculating frequency of occurrence for all pixel values 
-    for (row = 0; row < rows; row++) { 
-        // reading a row of image 
-        //read(input_file, &image[0], cols * sizeof(unsigned char)); 
-        //image[0] = 
-        for(int i=0; i<cols; i++){
+
+    // Declaring 2 arrays for storing histogram values (frequencies) and
+    // new gray level values (newly mapped pixel values as per algorithm)
+    int hist[256] = {0};
+    int new_gray_level[256] = {0};
+
+    // Declaring other important variables
+    int col, row, total, curr, i;
+
+    // allocating image array the size equivalent to number of columns
+    // of the image to read one row of an image at a time
+    // image = (unsigned char *)calloc(cols, sizeof(unsigned char));
+
+    // Calculating frequency of occurrence for all pixel values
+    for (row = 0; row < rows; row++)
+    {
+        // reading a row of image
+        //read(input_file, &image[0], cols * sizeof(unsigned char));
+        //image[0] =
+        for (int i = 0; i < cols; i++)
+        {
             image[i] = a.img[row + i];
         }
-        // logic for calculating histogram 
-        for (col = 0; col < cols; col++) 
-            hist[(int)a.img[row + col]]++; 
-    } 
-  
-    // calculating total number of pixels 
-    total = cols * rows; 
-  
-    curr = 0; 
-  
-    // calculating cumulative frequency and new gray levels 
-    for (i = 0; i < 256; i++) { 
-        // cumulative frequency 
-        curr += hist[i]; 
-  
-        // calculating new gray level after multiplying by 
-        // maximum gray count which is 255 and dividing by 
-        // total number of pixels 
-        new_gray_level[i] = round((((float)curr) * 255) / total); 
-    } 
-    
+        // logic for calculating histogram
+        for (col = 0; col < cols; col++)
+            hist[(int)a.img[row + col]]++;
+    }
+
+    // calculating total number of pixels
+    total = cols * rows;
+
+    curr = 0;
+
+    // calculating cumulative frequency and new gray levels
+    for (i = 0; i < 256; i++)
+    {
+        // cumulative frequency
+        curr += hist[i];
+
+        // calculating new gray level after multiplying by
+        // maximum gray count which is 255 and dividing by
+        // total number of pixels
+        new_gray_level[i] = round((((float)curr) * 255) / total);
+    }
+
     //Allocate for output
     struct buff newBuff;
     size_t size = newBuff.width * newBuff.height * newBuff.channels; // Allocate memory for result
-	unsigned char* result_img = malloc(size);
-	if (result_img == NULL) {
-		printf("Unable to allocate memory for the image.\n");
-		exit(1);
-	}
-  
-    // performing histogram equalisation by mapping new gray levels 
-    for (row = 0; row < rows; row++) { 
-        // reading a row of image 
-        //read(input_file, &image[0], cols * sizeof(unsigned char)); 
-        for(int i=0; i<cols; i++){
+                                                                     // unsigned char *result_img = malloc(size * sizeof(unsigned char));
+    unsigned char result_img[40000];
+    // if (result_img == NULL)
+    // {
+    //     printf("Unable to allocate memory for the image.\n");
+    //     exit(1);
+    // }
+
+    // performing histogram equalisation by mapping new gray levels
+    for (row = 0; row < rows; row++)
+    {
+        // reading a row of image
+        //read(input_file, &image[0], cols * sizeof(unsigned char));
+        for (int i = 0; i < cols; i++)
+        {
             image[i] = a.img[row + i];
         }
-        // mapping to new gray level values 
-        for (col = 0; col < cols; col++) 
-            image[col] = (unsigned char)new_gray_level[image[col]]; 
-  
-        // reading new gray level mapped row of image 
+        // mapping to new gray level values
+        for (col = 0; col < cols; col++)
+            image[col] = (unsigned char)new_gray_level[image[col]];
+
+        // reading new gray level mapped row of image
         //write(output_file, &image[0], cols * sizeof(unsigned char));
-        for(int i=0; i<cols; i++){
+        for (int i = 0; i < cols; i++)
+        {
             result_img[row + i] = a.img[row + i];
         }
     }
 
-    
-
     // Fill in info for buffer
-    char * nombre = a.imageName;
-	strcpy(newBuff.imageName, nombre);
+    char *nombre = a.imageName;
+    strcpy(newBuff.imageName, nombre);
 
     //Need to pass in extension
-	strcat(newBuff.imageName, "extension");
-    
-    //Change name
-    char * B_nombre = a.imageName;
-	strcpy(newBuff.name, B_nombre);
-	newBuff.width = a.width;
-	newBuff.height = a.height;
-	newBuff.channels = a.channels;
+    strcat(newBuff.imageName, "extension");
 
-    char* ext;
-	ext = strstr(a.imageName, ".png");
-	if (ext == NULL)
-		ext = strstr(a.imageName, ".jpg");
-	if (ext == NULL)
-		ext = strstr(a.imageName, ".tiff");
-	if (ext == NULL)
-		ext = strstr(a.imageName, ".gif");	// Use extension of a for extension of result
+    //Change name
+    char *B_nombre = a.imageName;
+    strcpy(newBuff.name, B_nombre);
+    newBuff.width = a.width;
+    newBuff.height = a.height;
+    newBuff.channels = a.channels;
+
+    char *ext;
+    ext = strstr(a.imageName, ".png");
+    if (ext == NULL)
+        ext = strstr(a.imageName, ".jpg");
+    if (ext == NULL)
+        ext = strstr(a.imageName, ".tiff");
+    if (ext == NULL)
+        ext = strstr(a.imageName, ".gif"); // Use extension of a for extension of result
 
     strcpy(newBuff.imageName, "histEQ");
-	strcat(newBuff.imageName, ext);
-	strcpy(newBuff.name, buffName);
+    strcat(newBuff.imageName, ext);
+    strcpy(newBuff.name, buffName);
 
+    newBuff.img = result_img;
 
-	
-
-	newBuff.img = result_img;
-
-    // freeing dynamically allocated memory 
-    free(image); 
-    // closing input and output files 
-    //close(input_file); 
+    // freeing dynamically allocated memory
+    // free(image);
+    // free(result_img);
+    // closing input and output files
+    //close(input_file);
 
     return newBuff;
-} 
+}

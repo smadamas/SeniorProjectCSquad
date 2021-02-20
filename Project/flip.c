114c2173f9584db469e9047f317ff763b96ab6d2
Ziyad Morsi <ziyad.morsi@loxleyservices.com>
2021-02-20 18:20:33 -0500

    flip vertical works

---
+++ Project/flip.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "libgd/src/gd.h"
+
+struct buff verticalFlip(struct buff a)
+{
+    struct buff temp = a;
+    // strcat(temp.imageName, a.imageName);
+    // strcpy(temp.name, a);
+    // temp.width = a.width;
+    // temp.height = a.height;
+    // temp.channels = a.channels;
+    int rows = temp.height / 2;
+    int row_stride = temp.width * temp.channels;
+    unsigned char *temp_row = (unsigned char *)malloc(row_stride);
+    int source_offset, target_offset;
+    for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+    {
+        source_offset = rowIndex * row_stride;
+        target_offset = (temp.height - rowIndex - 1) * row_stride;
+
+        memcpy(temp_row, temp.img + source_offset, row_stride);
+        memcpy(temp.img + source_offset, temp.img + target_offset, row_stride);
+        memcpy(temp.img + target_offset, temp_row, row_stride);
+    }
+    strcpy(temp.name, "flipV");
+    strcpy(temp.imageName, "flipV.jpg");
+    return temp;
+}

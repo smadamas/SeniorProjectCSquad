453b9a7ba3308f25acbceba5e7ea624bc34001fe
smadamas <i.am.sadams@gmail.com>
2021-03-08 19:04:47 -0500

    Start of Sharpening

--- Project/sharpen.c
+++ Project/sharpen.c
@@ -5,20 +5,19 @@
 #define KMAG  "\x1B[35m"
 #define RESET "\x1B[0m"
 
-struct buff blurr(struct buff buffer, int radius, double sigma)
+struct buff sharpen(struct buff buffer, double percent)
 {
     char newBuff[100];
     printf("Enter name of buffer: \n");
     gets(newBuff);
 
-    //printf("name:%s, rad:%d, sigma:%d\n", newBuff, radius, sigma);
     gdImagePtr copy;
     copy = gdImageClone(buffer.imrgb);
 
     struct buff result;
     char *temp = strtok(buffer.imageName, ".");
     char *ext = strtok(NULL, " ");
-    strcpy(result.imageName, "blurr-");
+    strcpy(result.imageName, "sharp-");
 	strcat(result.imageName, temp);
     strcat(result.imageName, ".");
     strcat(result.imageName, ext);
@@ -26,10 +25,17 @@ struct buff blurr(struct buff buffer, int radius, double sigma)
 	result.width = buffer.width;
 	result.height = buffer.height;
 	result.channels = buffer.channels;
-    result.imrgb = gdImageCopyGaussianBlurred(copy, radius, sigma);;
+
+    //Mean Image Removal Option
+    gdImageMeanRemoval(copy);
+
+    //Adding to edges option
+    
+
+    result.imrgb = copy;
     result.isLibgd = 1;
 
-    printf(KYEL "Done blurring!\n"RESET);
+    printf(KYEL "Done sharpening!\n"RESET);
 
     return result;
 }

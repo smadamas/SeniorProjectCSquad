0cd4860c283240cc2696d7b766bdeae9dec0fa14
Julian <jarvesu16@ufl.edu>
2022-03-31 13:50:42 -0400

    added error handling for nonexistent files/buffers

--- Project/errorcheck.c
+++ Project/errorcheck.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -10,11 +11,18 @@ bool checkImageFileExists(char* const fileName) //returns true if file exists (s
     struct stat buf;
     int fileStatus = stat(fileName, &buf);
     if(fileStatus == 0)
+    {
         return true;
+    }
     else
+    {
         return false;
+    }
 }
-bool checkBufferExists(char* const buffName)
+bool checkBufferExists(char* const bufferStatus)
 {
-
+    if(strcmp("false", bufferStatus) == 0)
+        return false;
+    else
+        return true;
 }
